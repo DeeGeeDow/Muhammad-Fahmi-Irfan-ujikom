0c51ac920f2644e45d250259d4a0a4b94e6dd18c
Muhammad Fahmi Irfan <13520152@std.stei.itb.ac.id>
2024-06-19 10:18:00 +0700

    feat : add animal

---
+++ Assets/Scripts/Animal/AnimalController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class AnimalController : MonoBehaviour
+{
+    public float speed;
+    public int HungerNeed;
+    public int Score;
+    public UnityEvent<int> AnimalFullEvent;
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.position += new Vector3(0, 0, -1) * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Food")
+        {
+            AnimalFullEvent.Invoke(Score);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
